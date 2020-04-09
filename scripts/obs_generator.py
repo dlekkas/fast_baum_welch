aabe3f24c3f996ee0fd90d0f07a5adc51fa31643
Fot_St <fotstrt95@gmail.com>
2020-04-09 04:18:21 +0300

    fix observation script and run validation source

--- scripts/obs_generator.py
+++ scripts/obs_generator.py
@@ -1,6 +1,8 @@
 import numpy as np
 import sys
 
+from HiddenMarkovModel import HiddenMarkovModel
+
 def generate_HMM_observation(num_obs, pi, T, E):
     def drawFrom(probs):
         return np.where(np.random.multinomial(1,probs) == 1)[0][0]
@@ -15,17 +17,17 @@ def generate_HMM_observation(num_obs, pi, T, E):
     return obs, states
 
 
-# number of states
+# number of observations
 N = int(sys.argv[1])
 
-# number of observations
+# number of states
 M = int(sys.argv[2])
 
 true_pi = np.random.dirichlet(np.ones(M),size=1)
 true_A = np.random.dirichlet(np.ones(M),size=M)
 true_B = np.random.dirichlet(np.ones(N),size=M)
 
-obs_seq, _ = generate_HMM_observation(50, true_pi, true_A, true_B)
+obs_seq, _ = generate_HMM_observation(50, true_pi, true_A, np.transpose(true_B))
 with open('../input/observations.txt', 'w') as f:
     for item in obs_seq[:-1]:
         f.write("%i " % item)
@@ -54,3 +56,15 @@ with open('../input/sample.txt', 'w') as f:
         for item in init_B[i][:-1]:
             f.write("%.4f " % item)
         f.write("%.4f\n" % init_B[i][-1])
+
+model = HiddenMarkovModel(init_A, np.transpose(init_B), init_pi[0], epsilon=0.0001, maxStep=12)
+trans0, transition, emission, c = model.run_Baum_Welch_EM(obs_seq, summary=False, monitor_state_1=True)
+
+print("Transition Matrix: ")
+print(transition)
+print()
+print("Emission Matrix: ")
+print(emission)
+print()
+print("Reached Convergence: ")
+print(c)
